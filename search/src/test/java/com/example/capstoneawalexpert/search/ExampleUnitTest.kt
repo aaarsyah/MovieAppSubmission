798097956376af3a995987e233ad65c1f5a8b0ec
Addinda Ayu Arsyah <addindaayuarsyah@gmail.com>
2025-01-04 02:40:37 +0700

    Update ExampleUnitTest.kt

--- search/src/test/java/com/example/capstoneawalexpert/search/ExampleUnitTest.kt
+++ search/src/test/java/com/example/capstoneawalexpert/search/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.capstoneawalexpert.search
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
